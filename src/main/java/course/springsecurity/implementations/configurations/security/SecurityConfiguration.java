f39b11589a4541931df02e7ac1b18c1799b0472b
israelemf <israelemf@outlook.com>
2024-03-19 23:58:36 -0300

    feat: Add users in memory in the application, a form of authentication for test environments

--- src/main/java/course/springsecurity/implementations/configurations/security/SecurityConfiguration.java
+++ src/main/java/course/springsecurity/implementations/configurations/security/SecurityConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         // Below is custom security configuration
@@ -19,5 +21,22 @@ public class SecurityConfiguration {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("root")
+                .authorities("total")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .authorities("basic")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
