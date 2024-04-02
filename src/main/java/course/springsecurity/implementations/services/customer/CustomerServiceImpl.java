e08fcb26b64772549cd602555cbf1631a842e144
israelemf <israelemf@outlook.com>
2024-04-01 22:36:07 -0300

    feat: Add hash to password on register user

--- src/main/java/course/springsecurity/implementations/services/customer/CustomerServiceImpl.java
+++ src/main/java/course/springsecurity/implementations/services/customer/CustomerServiceImpl.java
@@ -5,26 +5,34 @@ import course.springsecurity.implementations.entities.Customer;
 import course.springsecurity.implementations.repositories.CustomerRepository;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService{
     private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void save(CreateCustomerDto createCustomerDto) {
+        var customer = new Customer();
         String email = createCustomerDto.email();
+        String passwordHash = passwordEncoder.encode(createCustomerDto.password());
 
-        if (this.customerRepository.findByEmail(email).isPresent()) {
+        if (this.customerRepository.findCustomerByEmail(email).isPresent()) {
             throw new EntityExistsException("Esse email já está em uso. Tente outro.");
         }
 
-        var customer = new Customer();
         BeanUtils.copyProperties(createCustomerDto, customer);
+        customer.setPassword(passwordHash);
 
         this.customerRepository.save(customer);
     }
