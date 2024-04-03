ddce224aa5a31da5f0152cbe83505bb1db33660e
israelemf <israelemf@outlook.com>
2024-04-03 20:42:38 -0300

    feat: Change to use local database

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -1,6 +1,9 @@
 CREATE TABLE customers (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(45) NOT NULL,
+  customer_id SERIAL PRIMARY KEY,
+  name  VARCHAR(100) NOT NULL,
+  phone VARCHAR(20),
+  email VARCHAR(45) NOT NULL UNIQUE,
   password VARCHAR(200) NOT NULL,
-  role VARCHAR(45) NOT NULL
+  role VARCHAR(45) NOT NULL,
+  created_at DATE
 );
