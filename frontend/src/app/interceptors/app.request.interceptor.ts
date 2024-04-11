72973b55d1d9202d0c1b2367894b260ae1f2a28d
israelemf <israelemf@outlook.com>
2024-04-10 22:31:47 -0300

    feat: Add request and response of CSRF in frontend

--- frontend/src/app/interceptors/app.request.interceptor.ts
+++ frontend/src/app/interceptors/app.request.interceptor.ts
@@ -12,17 +12,26 @@ export class XhrInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     let httpHeaders = new HttpHeaders();
+
     if(sessionStorage.getItem('userdetails')){
       this.user = JSON.parse(sessionStorage.getItem('userdetails')!);
     }
+
     if(this.user && this.user.password && this.user.email){
       httpHeaders = httpHeaders.append('Authorization', 'Basic ' + window.btoa(this.user.email + ':' + this.user.password));
     }
 
+    let xsrf = sessionStorage.getItem("XSRF-TOKEN");
+    if (xsrf) {
+      httpHeaders = httpHeaders.append('X-XSRF-TOKEN', xsrf);
+    }
+
     httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
+
     const xhr = req.clone({
       headers: httpHeaders
     });
+
   return next.handle(xhr).pipe(tap(
       (err: any) => {
         if (err instanceof HttpErrorResponse) {
